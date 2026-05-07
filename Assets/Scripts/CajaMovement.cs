526287e259e550ac059381bf85853ae0d85de5ab
Eitan-Moscovich <49703153@est.ort.edu.ar>
2026-05-07 10:10:54 -0300

    si

---
+++ Assets/Scripts/CajaMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaMovement : MonoBehaviour
+{
+    public float velocidad = 3f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * velocidad * Time.deltaTime);
+    }
+}
